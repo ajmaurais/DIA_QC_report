3f613bab64ddb549d6ea8df826b73550ea118db6
mauraisa <mauraisa@uw.edu>
2024-04-25 16:41:16 -0700

    Add initial tests for parse_data

---
+++ tests/setup_functions.py
@@ -0,0 +1,60 @@
+
+import os
+import subprocess
+import inspect
+
+
+def make_work_dir(work_dir, clear_dir=False):
+    if not os.path.isdir(work_dir):
+        if os.path.isfile(work_dir):
+            raise RuntimeError('Cannot create work directory!')
+        os.makedirs(work_dir)
+    else:
+        if clear_dir:
+            for file in os.listdir(work_dir):
+                os.remove(f'{work_dir}/{file}')
+
+
+def run_command(command, wd):
+    result = subprocess.run(command, cwd=wd,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            shell=False, check=False)
+
+    prefix = f'{wd}/{inspect.stack()[1][3]}'
+    with open(f'{prefix}_command.txt', 'w') as outF:
+        outF.write(f"{' '.join(command)}\n")
+    with open(f'{prefix}.stdout.txt', 'w') as outF:
+        outF.write(f"{result.stdout.decode('utf-8')}\n")
+    with open(f'{prefix}.stderr.txt', 'w') as outF:
+        outF.write(f"{result.stderr.decode('utf-8')}\n")
+    with open(f'{prefix}.rc.txt', 'w') as outF:
+        outF.write(f'{str(result.returncode)}\n')
+    return result
+
+
+def setup_multi_db(data_dir, output_dir, group_by_gene=False):
+    make_work_dir(output_dir)
+    grouping = 'by_gene' if group_by_gene else 'by_protein'
+
+    commands = [['parse_data', '--projectName=Sp3',
+                 '-m', f'{data_dir}/metadata/Sp3_metadata.tsv',
+                 f'{data_dir}/skyline_reports/Sp3_replicate_quality.tsv',
+                 f'{data_dir}/skyline_reports/Sp3_{grouping}_precursor_quality.tsv'],
+                ['parse_data', '--overwriteMode=append', '--projectName=Strap',
+                 '-m', f'{data_dir}/metadata/Strap_metadata.tsv',
+                 f'{data_dir}/skyline_reports/Strap_replicate_quality.tsv',
+                 f'{data_dir}/skyline_reports/Strap_{grouping}_precursor_quality.tsv']]
+
+    if os.path.isfile(f'{output_dir}/data.db3'):
+        commands[0].insert(1, '--overwriteMode=overwrite')
+
+    if group_by_gene:
+        for i in range(len(commands)):
+            commands[i].insert(1, '--groupBy=gene')
+
+    results = list()
+    for command in commands:
+        results.append(run_command(command, output_dir))
+
+    return results
+
--- tests/test_parse_data.py
+++ tests/test_parse_data.py
@@ -1,10 +1,218 @@
 
 import unittest
-import subprocess
+import sqlite3
+import os
+import json
+
+import setup_functions
+
+from DIA_QC_report.submodules.metadata import Dtype
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-class TestParseData(unittest.TestCase):
-    def test_sucess(self):
+def get_db_metadata(db_path):
+    if not os.path.isfile(db_path):
+        raise RuntimeError(f"'{db_path}' does not exist!")
+
+    conn = sqlite3.connect(db_path)
+
+    # get replicates
+    query = 'SELECT replicateId, replicate, project FROM replicates;'
+    cur = conn.cursor()
+    cur.execute(query)
+    replicates = {row[0]: {'replicate': row[1], 'project': row[2]} for row in cur.fetchall()}
+
+    # get metadata
+    query = 'SELECT * FROM sampleMetadata;'
+    cur = conn.cursor()
+    cur.execute(query)
+
+    # pivot metadata wider
+    metadata = dict()
+    for row in cur.fetchall():
+        if row[0] not in metadata:
+            metadata[row[0]] = list()
+        metadata[row[0]].append((row[1], row[2]))
+
+    # get metadata types
+    query = 'SELECT annotationKey, annotationType FROM sampleMetadataTypes;'
+    cur = conn.cursor()
+    cur.execute(query)
+    types = {row[0]: row[1] for row in cur.fetchall()}
+
+    conn.close()
+
+    ret = {}
+    for replicateId, data in replicates.items():
+        ret[data['replicate']] = {'project': data['project']} | dict(metadata[replicateId])
+
+    return ret, types
+
+
+def load_metadata():
+    metadata = dict()
+    for project in ('Sp3', 'Strap'):
+        with open(f'{TEST_DIR}/data/metadata/{project}_metadata.json', 'r') as inF:
+            metadata[project] = json.load(inF)
+
+    metadata_table = {}
+    for project, replicates in metadata.items():
+        for replicate, annotations in replicates.items():
+            metadata_table[replicate] = {'project': project}
+            for variable, value in annotations.items():
+                metadata_table[replicate][variable] = value
+
+    return metadata_table
+
+
+METADATA = load_metadata()
+
+class TestMetadata(unittest.TestCase):
+    TEST_PROJECT = 'Sp3'
+
+    METADATA_TYPES = {'cellLine': 'STRING',
+                      'sample.name': 'STRING',
+                      'experiment': 'STRING',
+                      'NCI7.std': 'BOOL'}
+
+    @classmethod
+    def setUpClass(TestMetadata):
+        TestMetadata.work_dir = f'{TEST_DIR}/work/test_metadata'
+        setup_functions.make_work_dir(TestMetadata.work_dir, clear_dir=True)
+
+
+    @staticmethod
+    def setup_command(project, ext, meta_suffix='_metadata'):
+        db_path = f'test_{ext}{meta_suffix}.db3'
+        command = ['parse_data',
+                   f'--projectName={project}', '--overwriteMode=overwrite',
+                   '-m', f'{TEST_DIR}/data/metadata/{project}{meta_suffix}.{ext}',
+                   '-o', db_path,
+                   f'{TEST_DIR}/data/skyline_reports/{project}_replicate_quality.tsv',
+                   f'{TEST_DIR}/data/skyline_reports/{project}_by_protein_precursor_quality.tsv']
+        return command, db_path
+
+
+    def test_tsv(self):
+        command, db_path = self.setup_command(self.TEST_PROJECT, 'tsv')
+        result = setup_functions.run_command(command, TestMetadata.work_dir)
+
+        # test command was sucessful
+        self.assertEqual(0, result.returncode)
+
+        # get metadata from test db
+        db_metadata, db_metadata_types = get_db_metadata(f'{self.work_dir}/{db_path}')
+
+        # test all replicates are in test db
+        project_reps = [rep for rep, data in METADATA.items() if data['project'] == self.TEST_PROJECT]
+        self.assertEqual(len(project_reps), len(db_metadata))
+        for rep in project_reps:
+            self.assertTrue(rep in db_metadata)
+
+        # test all annotations are correct
+        for rep in project_reps:
+            self.assertDictEqual(db_metadata[rep], METADATA[rep])
+
+        # test metadata types are correct
+        for variable in db_metadata_types:
+            self.assertEqual(db_metadata_types[variable], self.METADATA_TYPES[variable])
+
+
+    def test_json(self):
+        command, db_path = self.setup_command(self.TEST_PROJECT, 'json')
+        result = setup_functions.run_command(command, TestMetadata.work_dir)
+
+        # test command was sucessful
+        self.assertEqual(0, result.returncode)
+
+        # get metadata from test db
+        db_metadata, db_metadata_types = get_db_metadata(f'{self.work_dir}/{db_path}')
+
+        # test all replicates are in test db
+        project_reps = [rep for rep, data in METADATA.items() if data['project'] == self.TEST_PROJECT]
+        self.assertEqual(len(project_reps), len(db_metadata))
+        for rep in project_reps:
+            self.assertTrue(rep in db_metadata)
+
+        # test all annotations are correct
+        for rep in project_reps:
+            self.assertDictEqual(db_metadata[rep], METADATA[rep])
+
+        # test metadata types are correct
+        for variable in db_metadata_types:
+            self.assertEqual(db_metadata_types[variable], self.METADATA_TYPES[variable])
+
+
+    def test_skyline_csv(self):
+        command, db_path = self.setup_command(self.TEST_PROJECT, 'csv', meta_suffix='_annotations')
+        result = setup_functions.run_command(command, TestMetadata.work_dir)
+
+        # test command was sucessful
+        self.assertEqual(0, result.returncode)
+
+        # get metadata from test db
+        db_metadata, db_metadata_types = get_db_metadata(f'{self.work_dir}/{db_path}')
+
+        # test all replicates are in test db
+        project_reps = [rep for rep, data in METADATA.items() if data['project'] == self.TEST_PROJECT]
+        self.assertEqual(len(project_reps), len(db_metadata))
+        for rep in project_reps:
+            self.assertTrue(rep in db_metadata)
+
+        # test all annotations are correct
+        for rep in project_reps:
+            self.assertDictEqual(db_metadata[rep], METADATA[rep])
+
+        # test metadata types are correct
+        for variable in db_metadata_types:
+            self.assertEqual(db_metadata_types[variable], self.METADATA_TYPES[variable])
+
+
+    def test_nomal_csv(self):
+        command, db_path = self.setup_command(self.TEST_PROJECT, 'json')
+        result = setup_functions.run_command(command, TestMetadata.work_dir)
+
+        # test command was sucessful
+        self.assertEqual(0, result.returncode)
+
+        # get metadata from test db
+        db_metadata, db_metadata_types = get_db_metadata(f'{self.work_dir}/{db_path}')
+
+        # test all replicates are in test db
+        project_reps = [rep for rep, data in METADATA.items() if data['project'] == self.TEST_PROJECT]
+        self.assertEqual(len(project_reps), len(db_metadata))
+        for rep in project_reps:
+            self.assertTrue(rep in db_metadata)
+
+        # test all annotations are correct
+        for rep in project_reps:
+            self.assertDictEqual(db_metadata[rep], METADATA[rep])
+
+        # test metadata types are correct
+        for variable in db_metadata_types:
+            self.assertEqual(db_metadata_types[variable], self.METADATA_TYPES[variable])
+
+    def test_infer_dtypes(self):
+        pass
+
+
+class TestMultiProject(unittest.TestCase):
+    @classmethod
+    def setUpClass(TestMultiProject):
+        TestMultiProject.work_dir = f'{TEST_DIR}/work/test_multi_project/'
+        TestMultiProject.db_path = f'{TestMultiProject.work_dir}/data.db3'
+        TestMultiProject.data_dir = f'{TEST_DIR}/data/'
+        TestMultiProject.results = setup_functions.setup_multi_db(TestMultiProject.data_dir,
+                                                                  TestMultiProject.work_dir)
+
+
+    def test_is_sucess(self):
+        for result in self.results:
+            self.assertEqual(0, result.returncode)
+
+
+    def test_metadata(self):
         pass
 
 
