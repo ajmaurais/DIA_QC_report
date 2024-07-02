dd6b8184c52177e097336c8d03bf04dcb540a044
mauraisa <mauraisa@uw.edu>
2024-07-02 12:41:30 -0700

    Add tests for single precursor report with 0 precursors found in all replicates.

--- tests/test_normalization_functions.py
+++ tests/test_normalization_functions.py
@@ -57,7 +57,7 @@ class TestNormalizationBase(setup_functions.AbstractTestsBase):
 
         manager = normalization.MedianNormalizer(self.conn)
         with self.assertLogs(normalization.LOGGER) as cm:
-            manager.normalize()
+            self.assertTrue(manager.normalize())
 
         precursors, proteins = manager.get_long_tables()
 
@@ -70,7 +70,7 @@ class TestNormalizationBase(setup_functions.AbstractTestsBase):
 
         manager = normalization.DirectlfqNormalizer(self.conn)
         with self.assertLogs(normalization.LOGGER) as cm:
-            manager.normalize()
+            self.assertTrue(manager.normalize())
 
         precursors, proteins = manager.get_long_tables()
 
@@ -82,7 +82,7 @@ class TestNormalizationBase(setup_functions.AbstractTestsBase):
 
         manager = normalization.MedianNormalizer(self.conn)
         with self.assertLogs(normalization.LOGGER) as cm:
-            manager.normalize()
+            self.assertTrue(manager.normalize())
 
         precursors, proteins = manager.get_long_tables(use_db_ids=False)
 
@@ -153,6 +153,90 @@ class TestSingleNormalization(unittest.TestCase, TestNormalizationBase):
             self.assertTrue(manager._read_precursors())
 
 
+class TestAllPrecursorsMissing(unittest.TestCase, TestNormalizationBase):
+    TEST_PROJECT = 'GFP'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.work_dir = f'{setup_functions.TEST_DIR}/work/test_normalization_functions_missing/'
+        cls.db_path = f'{cls.work_dir}/data.db3'
+        cls.data_dir = f'{setup_functions.TEST_DIR}/data/'
+
+        setup_functions.make_work_dir(cls.work_dir, True)
+
+        command = ['parse_data', f'--projectName={cls.TEST_PROJECT}',
+                   f'{cls.data_dir}/skyline_reports/{cls.TEST_PROJECT}_replicate_quality.tsv',
+                   f'{cls.data_dir}/skyline_reports/{cls.TEST_PROJECT}_precursor_quality.tsv']
+
+        cls.parse_result = setup_functions.run_command(command, cls.work_dir,
+                                                       prefix='parse_missing_precursors')
+
+        cls.conn = None
+        if cls.parse_result.returncode == 0:
+            if os.path.isfile(cls.db_path):
+                cls.conn = sqlite3.connect(cls.db_path)
+
+
+    def test_directlfq_normalization(self):
+        self.assertIsNotNone(self.conn)
+
+        manager = normalization.DirectlfqNormalizer(self.conn)
+        with self.assertLogs(normalization.LOGGER) as cm:
+            self.assertFalse(manager.normalize())
+
+        self.assertTrue('Can not perform DirectLFQ normalization with 0 precursors!' in cm.output[-1])
+
+
+    def test_read_precursors(self):
+        self.assertIsNotNone(self.conn)
+
+        # read ground truth data
+        df = pd.read_csv(f'{self.data_dir}/skyline_reports/{self.TEST_PROJECT}_precursor_quality.tsv', sep='\t')
+        df = df.rename(columns=PRECURSOR_QUALITY_REQUIRED_COLUMNS)
+        df = df[PRECURSOR_QUALITY_REQUIRED_COLUMNS.values()]
+        df = df[['replicateName', 'modifiedSequence', 'precursorCharge',
+                 'totalAreaFragment']].drop_duplicates()
+        df_w = df.pivot(index=['modifiedSequence', 'precursorCharge'],
+                        columns='replicateName', values='totalAreaFragment')
+
+        prec_counts = df_w.apply(lambda x: any(x.isnull().values), axis=1)
+        na_counts = Counter(prec_counts)
+        prec_counts = prec_counts[prec_counts == False]
+
+        n_missing = na_counts[True]
+        n_not_missing = na_counts[False]
+        n_precursors = sum(na_counts.values())
+
+        manager = normalization.MedianNormalizer(self.conn)
+        with self.assertLogs(normalization.LOGGER, level='WARNING') as cm:
+            self.assertTrue(manager._read_precursors())
+
+        # check that log shows correct number of missing values
+        self.assertTrue(f'Removed {n_missing} of {n_precursors} precursors with missing values.' in l for l in cm.output)
+        self.assertTrue(f'{n_not_missing} precursors without missing values remain.' in l for l in cm.output)
+
+        # check that db and ground truth precursor dfs match
+        # make gt dict
+        df_gt = df.set_index(['modifiedSequence', 'precursorCharge'])
+        df_gt = df_gt[df_gt.index.isin(prec_counts.index)]
+        df_gt = df_gt.reset_index()
+        df_gt = df_gt.set_index(['replicateName', 'modifiedSequence', 'precursorCharge'])
+        df_gt = {row.Index: row.totalAreaFragment for row in df_gt.itertuples()}
+
+        # make db dict
+        cur = self.conn.cursor()
+        cur.execute('SELECT id, replicate FROM replicates;')
+        rep_ids = {x[0]: x[1] for x in cur.fetchall()}
+
+        df_db = manager.precursors
+        df_db['replicateName'] = df_db['replicateId'].apply(lambda x: rep_ids[x])
+        df_db = df_db.set_index(['replicateName', 'modifiedSequence', 'precursorCharge'])
+        df_db = {row.Index: row.area for row in df_db.itertuples()}
+
+        self.assertEqual(len(df_db), len(df_gt))
+        self.assertDictEqual(df_db, df_gt)
+
+
 class TestMultiNormalization(unittest.TestCase, TestNormalizationBase):
     @classmethod
     def setUpClass(cls):
@@ -178,9 +262,11 @@ class TestMultiNormalization(unittest.TestCase, TestNormalizationBase):
             dfs[i] = pd.read_csv(dfs[i], sep='\t')
             dfs[i] = dfs[i].rename(columns=PRECURSOR_QUALITY_REQUIRED_COLUMNS)
             dfs[i] = dfs[i][PRECURSOR_QUALITY_REQUIRED_COLUMNS.values()]
-            dfs[i] = dfs[i][['replicateName', 'modifiedSequence', 'precursorCharge', 'totalAreaFragment']].drop_duplicates()
+            dfs[i] = dfs[i][['replicateName', 'modifiedSequence', 'precursorCharge',
+                             'totalAreaFragment']].drop_duplicates()
         df = pd.concat(dfs)
-        df_w = df.pivot(index=['modifiedSequence', 'precursorCharge'], columns='replicateName', values='totalAreaFragment')
+        df_w = df.pivot(index=['modifiedSequence', 'precursorCharge'],
+                        columns='replicateName', values='totalAreaFragment')
 
         prec_counts = df_w.apply(lambda x: any(x.isnull().values), axis=1)
         na_counts = Counter(prec_counts)
