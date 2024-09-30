50c2b4e2eaf0974069f6a3c7ab797d547efe0eaa
mauraisa <mauraisa@uw.edu>
2024-09-30 09:44:36 -0700

    Intermediate commit.
    Start using plottly for PCA plots in QC report.

--- src/submodules/pca_plot.py
+++ src/submodules/pca_plot.py
@@ -1,10 +1,14 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+
+# import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 from .dtype import Dtype
 
@@ -21,42 +25,71 @@ def pc_matrix(df):
     return pc, pc_var
 
 
-def pca_plot(pc, label_col, pc_var, label_type='discrete',
+def pca_plot(pc_unorm, label_col, pc_norm=None, label_type='discrete',
              fname=None, dpi=250, x_axis_pc=0, y_axis_pc=1, add_title=True):
+    
+    assert isinstance(pc_unorm, tuple)
+    (pc_unorm, pc_var_unorm) = pc_unorm
 
-    cmap = plt.get_cmap('viridis')
+    if pc_norm is not None:
+        assert isinstance(pc_norm, tuple)
+        (pc_norm, pc_var_norm) = pc_norm
 
-    if label_type == 'discrete':
-        colors = {label: color for color, label in zip(cmap(np.linspace(0, 1, len(pc[label_col].drop_duplicates()))),
-                                                       pc[label_col].drop_duplicates())}
+    # cmap = plt.get_cmap('viridis')
 
-        fig = plt.figure(figsize = (6, 4), dpi=dpi)
-        ax = fig.add_axes([0.1, 0.15, 0.65, 0.75])
+    # if label_type == 'discrete':
+    #     colors = {label: color for color, label in zip(cmap(np.linspace(0, 1, len(pc[label_col].drop_duplicates()))),
+    #                                                    pc[label_col].drop_duplicates())}
 
-        for label in sorted(pc[label_col].drop_duplicates()):
-            sele = pc[label_col] == label
-            ax.scatter(pc[sele][x_axis_pc], pc[sele][y_axis_pc], color=colors[label], label=label)
-        ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), title=label_col, alignment='left')
+    #     fig = plt.figure(figsize = (6, 4), dpi=dpi)
+    #     ax = fig.add_axes([0.1, 0.15, 0.65, 0.75])
 
-    elif label_type == 'continuous':
-        fig, ax = plt.subplots(1, 1, figsize=(6, 4), dpi=dpi)
-        cmap.set_bad(color='grey')
-        points=ax.scatter(pc[x_axis_pc], pc[y_axis_pc], c=pc[label_col], cmap=cmap, plotnonfinite=True)
-        fig.colorbar(points, label=label_col, use_gridspec=False,
-                     ticks=MaxNLocator(integer=True) if pd.api.types.is_integer_dtype(pc[label_col]) else None)
+    #     for label in sorted(pc[label_col].drop_duplicates()):
+    #         sele = pc[label_col] == label
+    #         ax.scatter(pc[sele][x_axis_pc], pc[sele][y_axis_pc], color=colors[label], label=label)
+    #     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), title=label_col, alignment='left')
+
+    # elif label_type == 'continuous':
+    #     fig, ax = plt.subplots(1, 1, figsize=(6, 4), dpi=dpi)
+    #     cmap.set_bad(color='grey')
+    #     points=ax.scatter(pc[x_axis_pc], pc[y_axis_pc], c=pc[label_col], cmap=cmap, plotnonfinite=True)
+    #     fig.colorbar(points, label=label_col, use_gridspec=False,
+    #                  ticks=MaxNLocator(integer=True) if pd.api.types.is_integer_dtype(pc[label_col]) else None)
 
     # axis labels
-    ax.set_xlabel(f'PC {x_axis_pc + 1} {pc_var[x_axis_pc]:.1f}% var')
-    ax.set_ylabel(f'PC {y_axis_pc + 1} {pc_var[y_axis_pc]:.1f}% var')
+    # ax.set_xlabel(f'PC {x_axis_pc + 1} {pc_var[x_axis_pc]:.1f}% var')
+    # ax.set_ylabel(f'PC {y_axis_pc + 1} {pc_var[y_axis_pc]:.1f}% var')
 
-    if add_title:
-        plt.title(f'Colored by {label_col}')
+    # fig = px.scatter(x=pc[x_axis_pc], y=pc[y_axis_pc], color=pc[label_col])
+    fig = make_subplots(rows=1, cols=(1 if pc_norm is None else 2))
 
-    if fname is None:
-        plt.show()
-    else:
-        plt.savefig(fname)
-    plt.close()
+    fig.add_trace(
+        go.Scatter(x=pc_unorm[x_axis_pc], y=pc_unorm[y_axis_pc],
+                   mode='markers', name='Unnormalized'),
+        row=1, col=1
+    )
+
+    if pc_norm is not None:
+        fig.add_trace(
+            go.Scatter(x=pc_norm[x_axis_pc], y=pc_norm[y_axis_pc],
+                       mode='markers', name='Normalized'),
+            row=1, col=2
+        )
+    
+    fig.update_layout(plot_bgcolor='white')
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+
+    fig.show()
+    
+    # if add_title:
+    #     plt.title(f'Colored by {label_col}')
+
+    # if fname is None:
+    #     plt.show()
+    # else:
+    #     plt.savefig(fname)
+    # plt.close()
 
 
 def convert_string_cols(df):
