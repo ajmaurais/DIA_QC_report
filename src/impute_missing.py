029431d57cecae261db59c8f0a171c748510c2ce
mauraisa <mauraisa@uw.edu>
2025-01-22 20:03:24 -0800

    Intermediate commit of impute_missing.
    Add get_manager function to handle both global and imputation
    method specific parameters.

--- src/impute_missing.py
+++ src/impute_missing.py
@@ -8,30 +8,108 @@ from .submodules.dia_db_utils import check_schema_version
 from .submodules.dia_db_utils import METADATA_TIME_FORMAT
 from .submodules.dia_db_utils import IS_IMPUTED, PRECURSOR_IMPUTE_METHOD, PROTEIN_IMPUTE_METHOD
 from .submodules.dia_db_utils import update_meta_value
+from .submodules.transformation import MethodOptions
 from .submodules.imputation import IMPUTATION_METHODS
+from .submodules.imputation import KNNImputer
 from .submodules.logger import LOGGER
 from . import __version__ as PROGRAM_VERSION
 
 COMMAND_DESCRIPTION = 'Impute missing precursor and/or protein values.'
 
+IMPUTE_DATA_OPTIONS = ('both', 'precursors', 'proteins')
+
+
+def get_manager(method, method_args, method_help=False, **kwargs):
+    '''
+    Get imputation manager and data processing metadata for imputation method.
+
+    Parameters
+    ----------
+    method: str
+        The name of the imputation method.
+    method_args: list
+        The list of method specific options strings.
+    method_help:
+        Show imputation method help and exit?
+    **kwargs: dict
+        Additional arguments passed to all imputation manager for all methods.
+
+    Raises
+    ------
+    SystemExit:
+        If method_help is True show method help and exit with code 0.
+        If method_args are invalid exit with code 1.
+    '''
+
+    metadata = kwargs.copy()
+    parser = MethodOptions()
+
+    if method == 'KNN':
+        parser.add_option('n_neighbors', default=5, min_value=1, min_inclusive=True, dtype=int,
+                          help_str='Number of neighboring replicates to use for imputation.')
+        parser.add_option('weights', choices=('uniform', 'weighted'), default='uniform', dtype=str,
+                          help_str='How to weigh the contribution of each neighbor on the '
+                                   'imputed value.')
+        Manager = KNNImputer
+    else:
+        raise ValueError(f"Unknown imputation method: '{method}'")
+
+    parser.description = f'Options for {Manager.__name__}'
+    if method_help:
+        parser.get_help(sys.stdout, max_width=os.get_terminal_size().columns)
+        sys.exit(0)
+
+    if not parser.parse_strings(method_args):
+        sys.exit(1)
+
+    manager_args = parser.get_option_dict()
+    manager = Manager(**manager_args, **kwargs)
+    metadata.update(manager_args)
+
+    return manager, metadata
+
 
 def parse_args(argv, prog=None):
     parser = argparse.ArgumentParser(prog=prog, description=COMMAND_DESCRIPTION)
 
-    norm_settings = parser.add_argument_group('Imputation settings')
-    norm_settings.add_argument('-m', '--method', choices=IMPUTATION_METHODS, default='k-means',
-                               help='Normalization method to use. Default is "k-means"')
-    norm_settings.add_argument('-l', '--level', choices=(0, 1), default=0,
-                               help='Which values to use for imputation. '
-                                    '0 for unnormalized, 1 for normalized.')
-    norm_settings.add_argument('--methodSettings', default=None, dest='method_settings',
-                               help='Imputation method specific settings having the form <key>=<value>.')
-    norm_settings.add_argument('--methodHelp', action='store_true', default=False,
-                               help='See imputation method specific settings.')
+    impute_settings = parser.add_argument_group('Global imputation settings')
+    impute_settings.add_argument('-i', '--imputeData', choices=IMPUTE_DATA_OPTIONS,
+                                 default='both', dest='impute_data',
+                                 help='Which data to impute. Default is "both".')
+    impute_settings.add_argument('-m', '--method', choices=IMPUTATION_METHODS, default='KNN',
+                                 help='Normalization method to use. Default is "k-means"')
+    impute_settings.add_argument('-l', '--level', choices=(0, 1), default=0,
+                                 help='Which values to use for imputation. '
+                                 '0 for unnormalized, 1 for normalized. Default is 0')
+    impute_settings.add_argument('-t', '--missingThreshold', default=0.5,
+                                 type=float, dest='missing_threshold',
+                                 help='The fraction of missing values above which imputation will be '
+                                      'skipped. 0.5 is the default, meaning that if more than 50%% of '
+                                      'values are missing in a group, the missing quantities for that '
+                                      'peptide/precursor will not be imputed.')
+    impute_settings.add_argument('--groupBy', choices=('all', 'project'),
+                                 default='project', dest='group_by',
+                                 help="How to group data for imputation. "
+                                      "If 'project', imputation is performed separately for each project. "
+                                      "If 'all' imputation is performed across all replicates. "
+                                      "'project' is the default.")
 
-    parser.add_argument('db', help='Path to sqlite batch/qc database.')
+    method_settings = parser.add_argument_group('Imputation method specific settings')
+    method_settings.add_argument('-s', '--methodSetting',
+                                 default=None, dest='method_settings', action='append',
+                                 help='Add method specific settings having the form: <key>=<value>')
+    method_settings.add_argument('--methodHelp', action='store_true', default=False,
+                                 dest='method_help', help='See imputation method specific settings.')
 
-    return parser.parse_args(argv)
+    parser.add_argument('db', nargs='?', default=None,
+                        help='Path to sqlite batch/qc database.')
+
+    args = parser.parse_args(argv)
+    if args.db is None and not args.method_help:
+        parser.print_usage()
+        parser.exit(1, f'{prog}: error: the following arguments are required: db\n')
+
+    return args
 
 
 def _main(args):
@@ -39,7 +117,27 @@ def _main(args):
     Actual main method. `args` Should be an initialized argparse namespace.
     '''
 
-    # init imputation manager
+    # initialize imputation manager
+    imputation_manager, metadata = get_manager(args.method, args.method_settings,
+                                               method_help=args.method_help,
+                                               impute_data=args.impute_data,
+                                               level=args.level,
+                                               missing_threshold=args.missing_threshold,
+                                               group_by_project=args.group_by == 'project')
+
+    if os.path.isfile(args.db):
+        conn = sqlite3.connect(args.db)
+    else:
+        LOGGER.error(f'Database file ({args.db}) does not exist!')
+        sys.exit(1)
+
+    # check database version
+    if not check_schema_version(conn):
+        sys.exit(1)
+
+    # add db connection to imputation_manager
+    imputation_manager.conn = conn
+
 
     # get commands previously run on db
 
---
+++ tests/test_impute_missing.py
@@ -0,0 +1,98 @@
+
+import os
+import re
+import unittest
+from unittest.mock import patch
+import sqlite3
+from io import StringIO
+
+import setup_functions
+
+import DIA_QC_report.submodules.dia_db_utils as db_utils
+import DIA_QC_report.submodules.imputation as imputaion
+import DIA_QC_report.impute_missing as impute_missing
+
+
+class TestGetManager(unittest.TestCase):
+    ERROR_PATTERNS = {'value':  r"Cannot convert value <[a-z]+> to <class '[a-z]+'>",
+                      'choice': r"Value '[a-z]+' must be in \('[a-z]",
+                      'range':  r'Value must be [<>]=?',
+                      'unknown_arg': r"Unknown option: '[a-z_]+'",
+                      'unparsable': r"Could not parse option argument: '[a-z_]+'"}
+
+
+    def do_valid_arg_test(self, method, method_args, method_kwargs, all_kwargs):
+        manager, metadata = impute_missing.get_manager(method, method_args, **method_kwargs)
+
+        for key, value in all_kwargs.items():
+            self.assertTrue(key in metadata)
+            self.assertEqual(value, metadata[key])
+            self.assertTrue(hasattr(manager, key))
+            self.assertEqual(getattr(manager, key), value)
+
+
+    def do_invalid_arg_test(self, method, method_args, error_re):
+        with self.assertLogs(impute_missing.LOGGER, level='ERROR') as log_cm:
+            with self.assertRaises(SystemExit) as error_cm:
+                impute_missing.get_manager(method, method_args)
+
+        self.assertEqual(error_cm.exception.code, 1)
+        self.assertTrue(any(re.search(error_re, m) for m in log_cm.output))
+
+
+    def do_invalid_kwarg_test(self, method, kwargs):
+        with self.assertRaises(TypeError) as cm:
+            impute_missing.get_manager(method, [], **kwargs)
+
+        error_msg = 'ImputationManagerBase.__init__()'
+        self.assertTrue(error_msg in str(cm.exception))
+
+
+    def test_method_help(self):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            with self.assertRaises(SystemExit) as cm:
+                impute_missing.get_manager('KNN', [], method_help=True)
+
+        self.assertEqual(cm.exception.code, 0)
+        mock_stdout.seek(0)
+        self.assertTrue('Options for KNNImputer' in mock_stdout.read())
+
+
+    def test_invalid_method(self):
+        with self.assertRaises(ValueError) as cm:
+            impute_missing.get_manager('Dummy', [], method_help=True)
+
+        self.assertTrue("Unknown imputation method: 'Dummy'" in str(cm.exception))
+
+
+    def test_KNN_invalid_args(self):
+        errors = [('choice', 'weights=none'),
+                  ('range', 'n_neighbors=0'),
+                  ('value', 'n_neighbors=wdummy'),
+                  ('unknown_arg', 'not_a_argument=0'),
+                  ('unparsable', 'weights')]
+
+        for err_type, arg in errors:
+            self.do_invalid_arg_test('KNN', [arg], self.ERROR_PATTERNS[err_type])
+
+
+    def test_KNN_valid_args(self):
+        args = [[['n_neighbors=6', "weights='uniform'"], {},
+                 {'n_neighbors': 6, 'weights': 'uniform'}],
+                [['n_neighbors=8', "weights='uniform'"],
+                 {'impute_data': 'precursors', 'missing_threshold': 0.6},
+                 {'n_neighbors': 8, 'weights': 'uniform'}],
+                [[], {'impute_data': 'proteins', 'missing_threshold': 0.75}, {}]]
+
+        for method_args, method_kwargs, target_args in args:
+            all_args = method_kwargs.copy()
+            all_args.update(target_args)
+
+            self.do_valid_arg_test('KNN', method_args, method_kwargs, all_args)
+
+
+    def test_KNN_inavlid_kwargs(self):
+        args = [{'dummy': 0}, {'level': 0, 'another_dummy': 1}]
+
+        for arg in args:
+            self.do_invalid_kwarg_test('KNN', arg)
